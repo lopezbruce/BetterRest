9670ae92d7353db3adbc0946542bfbd80b9e4657
Bruce Lopez <lopez.bruce@gmail.com>
2023-07-12 08:29:44 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Bruce Lopez on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
